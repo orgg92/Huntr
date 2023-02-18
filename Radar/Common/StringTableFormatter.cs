90bafa4f8fb5af055e23002e7cde454189483b2e
orgg92 <orgg92@gmail.com>
2023-02-18 22:00:03 +0000

    Changed logic to reuse same objects

--- Radar/Common/StringTableFormatter.cs
+++ Radar/Common/StringTableFormatter.cs
@@ -13,36 +13,39 @@ namespace Radar.Common
 
     public class StringTableFormatter
     {
-        public static string PadIP(string IP)
+        public static string PadIP(string IP, StringBuilder sb)
         {
+            sb.Clear();
+
             var count = IP.Length;
             var IPMaxLength = 15;
 
-            var sb = new StringBuilder();
             sb.Append("  ");
             sb.Append(IP);
             sb.Append(' ', IPMaxLength - count);
 
-
             return sb.ToString();
         }
 
-        public static string PadVendor(string vendor)
+        public static string PadVendor(string vendor, StringBuilder sb)
         {
+            sb.Clear();
+
             var count = 50;
-            var sb = new StringBuilder();
 
             sb.Append(" ");
             sb.Append(vendor);
             sb.Append(' ', count - vendor.Length);
+
             return sb.ToString();
         }
 
-        public static string PadHostname(string hostname, string tableHeaderMsg)
+        public static string PadHostname(string hostname, string tableHeaderMsg, StringBuilder sb)
         {
-            var count = tableHeaderMsg.Length;
-            var sb = new StringBuilder();
+            sb.Clear();
 
+            var count = tableHeaderMsg.Length;
+            
             sb.Append(" ");
             sb.Append(hostname);
             sb.Append(' ', (count - 3) - hostname.Length);
@@ -52,11 +55,13 @@ namespace Radar.Common
 
         public static Host PadPropertiesForDisplay(Host host, string tableHeaderMsg)
         {
+            var sb = new StringBuilder();
+
             var newHost = new Host()
             {
-                IP = PadIP(host.IP),
-                Vendor = PadVendor(host.Vendor),
-                HostName = PadHostname(host.HostName, tableHeaderMsg),
+                IP = PadIP(host.IP, sb),
+                Vendor = PadVendor(host.Vendor, sb),
+                HostName = PadHostname(host.HostName, tableHeaderMsg, sb),
                 MAC = host.MAC
             };            
                     
